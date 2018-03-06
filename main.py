548332abeb9cbed04f373befce0cbcf161f3bc75
StephanZuritaS <37113134+StephanZuritaS@users.noreply.github.com>
2018-03-06 14:45:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ t = Twython(app_key=KEY,
             app_secret=KEY_SECRET, 
             oauth_token=ACCESS_TOKEN, 
             oauth_token_secret=ACCESS_TOKEN_SECRET)
+
+query=input("Enter the hashtag:");
+query="".join(["#",query]);
+

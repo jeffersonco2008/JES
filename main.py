fdb59e45b5ef57901ef179e2114260ed14b923c6
StephanZuritaS <37113134+StephanZuritaS@users.noreply.github.com>
2018-03-06 14:37:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-TWITTER_APP_KEY = "Your Key Here"
-TWITTER_APP_KEY_SECRET = "Your Key Here" 
-TWITTER_ACCESS_TOKEN = "Your Key Here"
-TWITTER_ACCESS_TOKEN_SECRET = "Your Key Here"
+from twython import Twython
+
+KEY = "Your Key Here"
+KEY_SECRET = "Your Key Here" 
+ACCESS_TOKEN = "Your Key Here"
+ACCESS_TOKEN_SECRET = "Your Key Here"
+
+t = Twython(app_key=KEY, 
+            app_secret=KEY_SECRET, 
+            oauth_token=ACCESS_TOKEN, 
+            oauth_token_secret=ACCESS_TOKEN_SECRET)

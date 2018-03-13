e8b95b79537c658d8f498458a623b36dcb312a79
festeban26 <37111913+festeban26@users.noreply.github.com>
2018-03-12 20:41:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,33 @@ tweetsCountSearchLimit = 30
 search = twitter.search(q=hashtag, count=tweetsCountSearchLimit, result_type='recent')
 tweets = search['statuses']
 
+# Remove the retweets
+filteredTweets_withoutRT = []
+for tweet in tweets:
+    testString = tweet['text']
+    if testString[0:2] != "RT":
+        filteredTweets_withoutRT.append(tweet)
+tweets = filteredTweets_withoutRT
+
+print('Results for hashtag: ' + hashtag)
 # Documentation: https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
 for tweet in tweets:
     #tokens = word_tokenize(tweet['text'])
     #print(tokens)
 
+    # Tweet ID
     print('Tweet id: ', tweet['id_str'])
+    # Tweet text
     print('\ttweet:', tweet['text'])
+    # Original poster info [screen name & id]
     print('\toriginal poster:', '@' + tweet['user']['screen_name'] + '(id: ' + tweet['user']['id_str'] + ')')
+    # If it is a reply, show the OP screen name & id
     if tweet['in_reply_to_screen_name'] is not None:
         print('\tin reply to', '@' + tweet['in_reply_to_screen_name'] + '(id: ' + tweet['in_reply_to_user_id_str'] + ')')
+    # Tweet creation datetime
+    print('\tcreated at (UTC):', tweet['created_at'])
+    # Retweet count
     print('\tretweet count:', tweet['retweet_count'])
+    # Favorite count
     print('\tfavorite count:', tweet['favorite_count'])
     print('')
